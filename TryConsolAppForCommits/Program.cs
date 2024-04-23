6fd05603bed20436d9132d64afa535d56aa8bbfe
NikitaGostev <nik121283@gmail.com>
2024-04-23 21:32:41 +0300

    added code with Car class

--- TryConsolAppForCommits/Program.cs
+++ TryConsolAppForCommits/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TryConsolAppForCommits;
+
 Console.WriteLine("Hello, World!");
 
 //Add code for first commit
@@ -7,3 +9,15 @@ Console.WriteLine("First commit");
 
 //Add code for second commit
 Console.WriteLine("Second commit");
+
+Car car1 = new Car();
+car1.MaxSpeed = 10;
+
+Car car2 = new Car();
+car2.MaxSpeed = 10;
+
+Console.WriteLine(car1.Equals(car2));
+
+Console.WriteLine(car1.GetHashCode());
+Console.WriteLine(car2.GetHashCode());
+
